94cf9816429efce65906384864a429aaa2ad52cd
Kinevich Konstantin <kinevich.k.a@gmail.com>
2020-11-27 23:16:30 +0300

    Callback

---
+++ Callback/Callback/Extension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Callback
+{
+    public static class Extension
+    {
+        public static bool MyAny(this int[] array, Func<int, bool> predicate)
+        {
+            foreach (int i in array)
+            {
+                if (predicate(i))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool MyAll(this int[] array, Func<int, bool> predicate)
+        {
+            foreach (int i in array)
+            {
+                if (!predicate(i))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Callback/Callback/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Callback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[] { -2, 6, 8 };
+            Console.WriteLine(ints.MyAny(i => i % 2 == 0 && i < 0));
+            Console.WriteLine(ints.MyAll(i => i % 2 == 0));
+        }
+    }
+}
