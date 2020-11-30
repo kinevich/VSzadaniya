8516c354450daae100113c70777f0ea9b4a196a3
Kinevich Konstantin <kinevich.k.a@gmail.com>
2020-11-30 22:28:19 +0300

    StringFinder

---
+++ StringFinder/StringFinder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace StringFinder
+{
+    class StringFinder
+    {
+        public delegate void Del(string name, int num);
+        public event Del Found;
+
+        public void Start(string directoryPath, string text)
+        {
+            foreach (string fileName in Directory.GetFiles(directoryPath))
+            {
+                string filePath = Path.GetFullPath(fileName);
+                if (Path.GetExtension(filePath) == ".txt")
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    var lineNumber = Array.IndexOf(lines, text);
+                    if (lineNumber >= 0)
+                        Found(fileName, lineNumber);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"E:\Test_StringFinder";
+            string text = "text";
+            StringFinder stringFinder = new StringFinder();
+
+            stringFinder.Found += StringFinder_Found;
+
+            stringFinder.Start(directoryPath, text);
+        }
+
+        private static void StringFinder_Found(string name, int num)
+        {
+            Console.WriteLine($"TEXT FOUND. File: {name}. Line number: {num}.");
+        }
+    }
+
+}
