a81df48ff80aad7e577d57f1721d28eb6361bfde
Kinevich Konstantin <kinevich.k.a@gmail.com>
2020-12-04 17:07:48 +0300

    Traverse

---
+++ TraversingDirectoryTrees/TraversingDirectoryTrees/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TraversingDirectoryTrees
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"";
+            Traverse(path);
+        }
+
+        private static void Traverse(string path)
+        {
+            string[] directories = Directory.GetDirectories(path);
+
+            if (directories == null || directories.Length == 0)
+                return;
+
+            foreach (string dir in directories)
+            {
+                Console.WriteLine(dir);
+                Traverse(dir);
+            }
+        }
+    }
+}
