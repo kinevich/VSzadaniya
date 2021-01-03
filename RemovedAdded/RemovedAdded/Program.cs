22dfe7d4d13c9e21742d1ee1bee2e156d7899013
Kinevich Konstantin <kinevich.k.a@gmail.com>
2021-01-03 19:55:07 +0300

    RemovedAdded

--- RemovedAdded/RemovedAdded/Program.cs
+++ RemovedAdded/RemovedAdded/Program.cs
@@ -8,99 +8,51 @@ namespace RemovedAdded
     {
         static void Main(string[] args)
         {
-            Service service = new Service();
-
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-            dict.Add(4, 5);
-            dict.Add(5, 5);
-            dict.Add(3, 5);
-            dict.Add(1, 5);
-            dict.Add(6, 5);
-            dict.Add(10, 5);
-            Dictionary<int, int> clonedDict = new Dictionary<int, int>(dict);
+            IEnumerable<int> collection = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            IEnumerable<int> modifiedCollection = new int[] { 1, 2, 3, 8, 9, 10 };
 
             Console.WriteLine("Original Dictionary");
-            foreach (KeyValuePair<int, int> kvp in dict)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
+            Array.ForEach(collection.ToArray(), Console.WriteLine);
             Console.WriteLine();
-
-            Dictionary<int, int> modifiedDict = service.Modify(clonedDict);
 
             Console.WriteLine("Modified Dictionary");
-            foreach (KeyValuePair<int, int> kvp in modifiedDict)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
+            Array.ForEach(modifiedCollection.ToArray(), Console.WriteLine);
             Console.WriteLine();
 
-            Tuple<Dictionary<int, int>, Dictionary<int, int>> tuple = GetRemovedAndAdded(dict, modifiedDict);
+            Tuple<IEnumerable<int>, IEnumerable<int>> tuple =
+                GetAddedAndRemoved(collection, modifiedCollection);
 
             Console.WriteLine("Added");
-            foreach (KeyValuePair<int, int> kvp in tuple.Item1)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
+            Array.ForEach(tuple.Item1.ToArray(), Console.WriteLine);
             Console.WriteLine();
 
             Console.WriteLine("Removed");
-            foreach (KeyValuePair<int, int> kvp in tuple.Item2)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
+            Array.ForEach(tuple.Item2.ToArray(), Console.WriteLine);
             Console.WriteLine();
         }
 
-        private static Tuple<Dictionary<int, int>, Dictionary<int, int>> GetRemovedAndAdded
-            (Dictionary<int, int> originalDict, Dictionary<int, int> modifiedDict)
+        private static Tuple<IEnumerable<int>, IEnumerable<int>> GetAddedAndRemoved
+            (IEnumerable<int> originalCollection, IEnumerable<int> modifiedCollection)
         {
-            Dictionary<int, int> removed = new Dictionary<int, int>();
-            Dictionary<int, int> added = new Dictionary<int, int>();
+            HashSet<int> added = new HashSet<int>();
+            HashSet<int> removed = new HashSet<int>();
 
-            foreach (KeyValuePair<int, int> kvp in originalDict)
+            HashSet<int> original = originalCollection.ToHashSet();
+            HashSet<int> modified = modifiedCollection.ToHashSet();
+
+            foreach (int item in original)
             {
-                if (!modifiedDict.ContainsKey(kvp.Key))
-                    removed.Add(kvp.Key, kvp.Value);
+                if (!modified.Contains(item))
+                    removed.Add(item);
             }
 
-            foreach (KeyValuePair<int, int> kvp in modifiedDict)
+            foreach (int item in modified)
             {
-                if (!originalDict.ContainsKey(kvp.Key))
-                    added.Add(kvp.Key, kvp.Value);
+                if (!original.Contains(item))
+                    added.Add(item);
             }
 
-            return new Tuple<Dictionary<int, int>, Dictionary<int, int>>(added, removed);
-        }
-    }
-
-    class Service
-    {
-        public Dictionary<int, int> Modify(Dictionary<int, int> dict)
-        {
-            Random random = new Random();
-
-            if (dict.Count > 0)
-            {
-                int numberOfAdded = random.Next(dict.Count);
-                for (int i = 1; i <= numberOfAdded; ++i)
-                {
-                    dict.Add(random.Next(i + 4234), random.Next(i + 3432));
-                }
-            }
-
-            if (dict.Count > 0)
-            {
-                int numberOfRemoved = random.Next(dict.Count);
-                for (int i = 1; i <= numberOfRemoved; ++i)
-                {
-                    int randomIndex = random.Next(dict.Count);
-                    int randomKey = dict.Keys.ElementAt(randomIndex);
-                    dict.Remove(randomKey);
-                }
-            }
-
-            return dict;
+            return new Tuple<IEnumerable<int>, IEnumerable<int>>(added, removed);
         }
     }
 }
