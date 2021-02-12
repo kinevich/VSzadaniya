70f4cd6007c7dc37d34bf94ec47fe39c9ffe0b73
Kinevich Konstantin <kinevich.k.a@gmail.com>
2021-02-12 03:15:45 +0300

    AbstractFactory

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Room
+    {
+        public IChandelier Chandelier { get; }
+
+        public IWallpaper Wallpaper { get; }
+
+        public Room (IRoomFactory factory)
+        {
+            Chandelier = factory.CreateChandelier();
+            Wallpaper = factory.CreateWallpaper();
+        }
+    }
+
+    class LightRoomFactory : IRoomFactory
+    {
+        public IChandelier CreateChandelier()
+        {
+            return new LightRoomChandelier();
+        }
+
+        public IWallpaper CreateWallpaper()
+        {
+            return new LightRoomWallpaper();
+        }
+    }
+
+    class LightRoomChandelier : IChandelier { }
+
+    class LightRoomWallpaper : IWallpaper { }
+
+    class DarkRoomFactory : IRoomFactory
+    {
+        public IChandelier CreateChandelier()
+        {
+            return new DarkRoomChandelier();
+        }
+
+        public IWallpaper CreateWallpaper()
+        {
+            return new DarkRoomWallpaper();
+        }
+    }
+
+    class DarkRoomChandelier : IChandelier { }
+
+    class DarkRoomWallpaper : IWallpaper { }
+
+    interface IChandelier { }        
+    
+
+    interface IWallpaper { }
+
+
+    interface IRoomFactory
+    {
+        IChandelier CreateChandelier();
+        IWallpaper CreateWallpaper();
+    }
+
+}
