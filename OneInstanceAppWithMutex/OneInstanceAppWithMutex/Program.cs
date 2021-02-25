a09fa812eb5e6d645e042c82fd226dfdd0353dde
Kinevich Konstantin <kinevich.k.a@gmail.com>
2021-02-25 11:25:48 +0300

    OneInstanceAppWithMutex

---
+++ OneInstanceAppWithMutex/OneInstanceAppWithMutex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace OneInstanceAppWithMutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool createdNew;
+
+            new Mutex(true, "Mutex", out createdNew);
+
+            if (!createdNew)
+            {
+                Console.WriteLine("App is already running! Exiting the application.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Continuing with the application");
+            Console.ReadKey();
+        }
+    }
+}
