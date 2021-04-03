880613a901d920ffb97856951dc8f52bc6424267
Kinevich Konstantin <kinevich.k.a@gmail.com>
2021-04-03 17:32:09 +0300

    InAndOut

---
+++ InAndOut/InAndOut/Migrations/20210330182849_EdditAmountFromDoubleToDecimal.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class EdditAmountFromDoubleToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Expenses",
+                type: "decimal(18,4)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Expenses",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)");
+        }
+    }
+}
---
+++ InAndOut/InAndOut/Models/Expense.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Expense name")]
+        [Required]
+        public string ExpenseName { get; set; }
+
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Amount { get; set; }
+
+        [DisplayName("Expense type")]
+        public int ExpenseTypeId { get; set; }
+
+        [ForeignKey("ExpenseTypeId")]
+        public virtual string ExpenseType { get; set; }
+    }
+}
