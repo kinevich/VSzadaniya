28f1a32988a2e3abd0214423b9f00ab0d5ec3905
Kinevich Konstantin <kinevich.k.a@gmail.com>
2021-06-25 15:29:03 +0300

    WebImageConvolution

---
+++ WebImageConvolution/WebImageConvolution/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebImageConvolution.Models;
+
+namespace WebImageConvolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly string _dir;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+            _dir = _env.ContentRootPath;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SingleFile(IFormFile file)
+        {
+            using (var fileStream = new FileStream(Path.Combine(_dir, "file.png"), FileMode.Create, FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
